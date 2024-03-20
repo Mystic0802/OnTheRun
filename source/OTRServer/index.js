b8657e4033012faa34b3cbc3a18dc7c9381e6842
YammyToast <cyanjamesmail@gmail.com>
2024-03-20 21:53:17 +0000

    Display Environment Setup

--- source/OTRServer/index.js
+++ source/OTRServer/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+var displayRouter = require('./routes/display');
+app.use('/display', displayRouter);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.set('views', path.join(__dirname, 'views'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
