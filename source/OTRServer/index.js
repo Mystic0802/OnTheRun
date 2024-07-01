34813bf8e9875f20685d9d9dde799a5dd14a5c29
YammyToast <cyanjamesmail@gmail.com>
2024-07-01 18:31:03 +0100

    server-side docstrings

--- source/OTRServer/index.js
+++ source/OTRServer/index.js
@@ -46,10 +46,10 @@ class Player {
 // ====================================================================================
 
 /**
- * __! The variable: state (lowercase) is the server's running instance of this enumeration. !__ 
- * 
+ * __! The variable: state (lowercase) is the server's running instance of this enumeration. !__
+ *
  * __! This enumeration differs slightly in elements to the states used in the client-side states.js. !__
- * 
+ *
  * Enumeration of the finite states the server can exist in.
  * Each state represents a distinct status of the game indicative of its name.
  *
@@ -72,21 +72,28 @@ const State = Object.freeze({
 
 /**
  *  __! This enumeration differs slightly in elements to the states used in the client-side states.js. !__
- * 
- * __! See @function resolve_state for  !__
- * 
+ *
+ * __! See 'resolve_state' method for specific transition implementations/logic. !__
+ *
  * Enumeration of the transitions through which the server's state can be changed. Each transition acts as a coin which permits a state to mutate in a defined manner
  * into another state.
- * 
- * For example, the INIT State can transition into the JOIN State if a valid 
- * 
+ *
+ * For example, the INIT State can transition into the JOIN State if a INIT_DONE transition is also passed to the 'resolve_state' method.
+ *
  */
 const Transitions = Object.freeze({
   INIT_DONE: Symbol("init_done"),
   JOIN_DONE: Symbol("join_done"),
 });
 
+/**
+ * Implements piece-wise logic to resolve how the 'input state' should mutate given the 'input transition'
+ * @param {*} __input_state: Input state of the transition. This will normally be the server's state instance.
+ * @param {*} __input_transition: Input transition for the mutation.
+ * @returns
+ */
 function resolve_state(__input_state, __input_transition) {
+  // due to the two-half combination, a switch statement would not be a appropriate here.
   if (
     __input_transition == Transitions.INIT_DONE &&
     __input_state == State.INIT
@@ -100,18 +107,49 @@ function resolve_state(__input_state, __input_transition) {
   else return __input_state;
 }
 
-// THE STATE!!!1!!!
+/**
+ * The server's main state instance. It's type should always be some enumeration of the 'State' (uppercase) list.
+ *
+ * The intial state is State.INIT to ensure correct logic. If it's initial value is not INIT, it has likely been changed in testing
+ * and should therefore be changed back.
+ */
 let state = State.INIT;
-// uuid -> Player (obj)
+/**
+ * __Mapping Reference:__
+ *
+ * __UUID__ (string) -> __Player__ (class instance)
+ *
+ * ---
+ * The server's main hashmap containing references for all players/contestants currently in the game. Each item in the map
+ * represents a distinct player that is potentially connected through multiple socket ids.
+ *
+ * Each element is mapped by a UUID key, which is also stored internally by the Player class.
+ */
 let players = new Map();
-// socket -> uuid
-let socket_uuid_map = new Map();
+/**
+ * Independent variable that stores the Player instance for the player that is the chaser.
+ */
 let player_chaser;
+/**
+ * __Mapping Reference:__
+ *
+ * __Socket__ (string) -> __UUID__ (string)
+ *
+ * ---
+ * Utility hashmap that maps Socket.io/Websocket UUIDs of clients onto their UUID.
+ */
+let socket_uuid_map = new Map();
 
 // ====================================================================================
 // Websocket Logic
 // ====================================================================================
 
+/**
+ * Helper function for creating state/data messages.
+ * @param {*} __state: State for the client to now become.
+ * @param {*} __data : State-wise data to be attached.
+ * @returns Object to be emitted over Socket.io.
+ */
 function create_message(__state, __data) {
   return {
     timestamp: Date.now(),
@@ -120,6 +158,10 @@ function create_message(__state, __data) {
   };
 }
 
+/**
+ * Helper function for gather display names for all players in the player-hashmap.
+ * @returns List of display name strings.
+ */
 function get_player_names() {
   let a = [];
   // value, key
@@ -129,7 +171,11 @@ function get_player_names() {
   return a;
 }
 
-// checks whether the game_start state should be done.
+/**
+ * Procedure to check whether the server should transition from the JOIN state into the main game.
+ *
+ * Called every time a player_create or chaser_create message is successfully processed.
+ */
 function check_ready() {
   // lobby full logic
   console.log(`players size: ${players.size}, chaser: ${player_chaser}`);
@@ -145,14 +191,17 @@ function check_ready() {
   io.emit("state", msg);
 }
 
-// Socket logic.
+// Begin Socket.io logic
 io.on("connection", (socket) => {
   console.log(`user connected: ${socket.id}`);
 
-  // display_get init logic.
-  // server discovering display for the first time
-  // OR
-  // display recovering state on a crash or something
+  /**
+   * Display initialization AND recovery logic.
+   *
+   * If the display has not yet been connected, a state transition is called.
+   * OTHERWISE
+   * Data is transmitted to the display client in order to reinstate the current session, in a similar manner to player session recovery.
+   */
   socket.on("display_get", (__msg, callback) => {
     try {
       // If this is the discovery, can now move forwards state.
@@ -180,9 +229,12 @@ io.on("connection", (socket) => {
   // PLAYER CREATION
   // ====================================================================================
 
-  // player recovery requests
-  // to-do, actually sync it.
-  // CHASER STUFF, ONLY PLAYER WORKS RN
+  /**
+   * Existing player recovery/resync logic.
+   *
+   * A client can make a recovery request if it has a session_id stored locally (cookies).
+   * If valid, data required to reinstate the client is transmitted, such as the game state.
+   */
   socket.on("player_get", (__msg, callback) => {
     try {
       let msg_data = __msg;
@@ -232,7 +284,13 @@ io.on("connection", (socket) => {
     }
   });
 
-  // create player requests
+  /**
+   * Player/Contestant Creation Logic.
+   *
+   * Handles player creation requests if the server is currently in the JOIN state.
+   *
+   * ! This logic does not encompass chaser creation logic.
+   */
   socket.on("player_create", (__msg, callback) => {
     // don't let people join if not in the join state
     if (state != State.JOIN) {
@@ -267,6 +325,11 @@ io.on("connection", (socket) => {
     }
   });
 
+  /**
+   * Chaser Creation Logic.
+   *
+   * Handles chaser creation requests if the server is currently in the JOIN state.
+   */
   socket.on("chaser_create", (__msg, callback) => {
     if (state != State.JOIN) {
       console.error(
@@ -307,7 +370,11 @@ io.on("connection", (socket) => {
   // DISCONNECT
   // ====================================================================================
 
-  // disconnect logic.
+  /**
+   * Client Disconnect Logic
+   *
+   * Handles the necessary overhead of a player disconnecting at any state of the game.
+   */
   socket.on("disconnect", (__msg, callback) => {
     // try remove player from lists
     // only clear the lists if the game is in the joining state
