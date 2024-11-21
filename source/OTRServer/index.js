2e103de41655cd9defb7245de75de43faf356ef2
Nathan Webb <NathanWebb02@outlook.com>
2024-11-21 03:33:47 +0000

    basic file structure + placeholder views

--- source/OTRServer/index.js
+++ source/OTRServer/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '..', 'OTRDisplay')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, '..', 'OTRDisplay', 'Views', 'Home.html'));
+});
+
+app.get('/Lobby', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'OTRDisplay', 'Views', 'Lobby.html'));
+});
+
+app.get('/Admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'OTRDisplay', 'Views', 'Admin.html'));
 });
 
 app.listen(port, () => {
